9c753fcc8bd2da9b0a4c0496335d3b837afc9060
Brad Kuhne <bjkuhne@aol.com>
2022-02-01 16:37:25 -0600

    added default database query

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const mysql = require("mysql2");
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'bkuhne',
+      // Your MySQL password
+      password: 'BKBootcamp4667#',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 
 // Test that a rout is working.  Can be removed
 // app.get('/', (req, res) => {
@@ -11,6 +27,11 @@ app.use(express.json());
 //         message: "Hello World"
 //     });
 // });
+
+// DEFAULT MYSQL QUERY SYNTAX BELOW!!!
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 
 // Default response for any other request (Not Found)
